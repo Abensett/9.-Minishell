2a34432bda99f954c4b3511bb45a5afeef435217
Abensett <bensettiaimen@mailo.com>
2022-05-07 13:38:59 +0200

    s

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 14:24:25 by abou              #+#    #+#             */
-/*   Updated: 2022/04/28 05:39:45 by abensett         ###   ########.fr       */
+/*   Updated: 2022/05/07 13:17:12 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@
 
 typedef struct s_simple_command
 {
-	int		nb_arguments;
+	int		nb_args;
 	char	**av;
 }			t_simple_command;
 
@@ -72,6 +72,26 @@ typedef struct s_minishell
 	t_simple_command	*cmds;
 	t_env_list			*env;
 }						t_minishell;
+/*The space handling*/
+void	space_handler(char **line);
+
+/*The quotes handling*/
+void		quote_remove(t_env_list **env, char **line);
+
+/*The expansion*/
+void		expansion(t_env_list **env, char **line, int *i);
+
+/*The arglist aka lexer.c*/
+t_list				*lexer(t_env_list **env, char *line);
+void				type_quote(char *quote, char c);
+
+/*The parsing utils*/
+void				parser(t_list **token_list, t_minishell *shell);
+
+/*The parsing utils*/
+int					cmd_counter(char *str, t_list *token_lst);
+int					args_counter(t_list *token_list);
+t_simple_command	*fill_cmds(t_list *t, t_simple_command *cmds, t_minishell *shell);
 
 /* The executor.c*/
 void		execution(int i, t_minishell *shell);
@@ -84,6 +104,7 @@ t_env_list	*init_env(char **envp);
 char		**set_envp(t_env_list *env);
 
 /* The ft_envs2.c*/
+char		*get_env(t_env_list **env, char *key);
 void		set_env(t_minishell *shell, char *str);
 void		unset_env(t_minishell *shell, char *key);
 
@@ -97,5 +118,6 @@ void		pwd(void);
 void		export(int i, t_minishell *shell);
 void		unset(int i, t_minishell *shell);
 void		env(t_minishell *shell);
+void	ft_exit(int i, t_minishell *shell);
 
 #endif
