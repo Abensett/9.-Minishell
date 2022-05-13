45ee716b7222acfc1813e38667425a98eff02d56
Abensett <bensettiaimen@mailo.com>
2022-05-13 09:26:53 +0200

    d

--- last_version/includes/minishell.h
+++ last_version/includes/minishell.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/01 14:24:25 by abou              #+#    #+#             */
-/*   Updated: 2022/05/09 12:14:21 by abensett         ###   ########.fr       */
+/*   Created: 2022/04/01 14:24:25 by abensett          #+#    #+#             */
+/*   Updated: 2022/05/12 15:28:07 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,8 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 
-// /*LISTE CHAINEES BLOCK = FUNCTIONS+OPTION+ARG  PAS UTILISE POUR L'INSTANT*/
-
-// typedef struct s_all_commands
-// {
-// 	char					**cmds;
-// 	struct s_all_commands	*next;
-// }			t_all_commands;
+/* global variable : self-explanatory */
+extern int	g_exit_status;
 
 /* Donne le nombre d'arguments + 1 car nom fonction + args */
 
@@ -78,7 +73,6 @@ typedef struct s_minishell
 	t_env_list			*env;
 }						t_minishell;
 
-
 //////////////////////////  LEXER  //////////////////////////////////
 /*The space handling*/
 void				space_handler(char **line);
@@ -96,50 +90,63 @@ void				type_quote(char *quote, char c);
 
 //////////////////////////  PARSING  ////////////////////////////////
 /*The valid.c*/
-int		is_valid(t_list *token_list);
+int					is_valid(t_list *token_list);
 
 /*The parsing utils*/
 void				parser(t_list **token_list, t_minishell *shell);
 
 /*The parsing utils*/
 int					cmd_counter(char *str, t_list *token_lst);
+t_simple_command	*pipe_separation(t_list **token_list, t_minishell *shell);
 int					args_counter(t_list *token_list);
-t_simple_command	*fill_cmds(t_list *t, t_simple_command *cmds, t_minishell *shell);
+t_simple_command	*fill_cmds(t_list *t, t_simple_command *cmds,
+						t_minishell *shell);
 
 //////////////////////////  EXECUTION  ////////////////////////////////
 
 /* The executor.c*/
-void		execution(int i, t_minishell *shell);
-void		executor(t_minishell *shell);
+void				execution(int i, t_minishell *shell, t_exec *exec);
+void				executor(t_minishell *shell);
 
 /*The heredoc.*/
-int		heredoc(t_minishell *shell);
-
+int					heredoc(t_minishell *shell);
 
 //////////////////////////  SUBSYSTEM  ////////////////////////////////
 
-
 /* The ft_envs.c*/
-t_env_list	*env_new(char *str);
-void		env_add_back(t_env_list **env, t_env_list *new);
-t_env_list	*init_env(char **envp);
-char		**set_envp(t_env_list *env);
+t_env_list			*env_new(char *str);
+void				env_add_back(t_env_list **env, t_env_list *new);
+t_env_list			*init_env(char **envp);
+char				**set_envp(t_env_list *env);
 
 /* The ft_envs2.c*/
-char		*get_env(t_env_list **env, char *key);
-void		set_env(t_minishell *shell, char *str);
-void		unset_env(t_minishell *shell, char *key);
+char				*get_env(t_env_list **env, char *key);
+void				set_env(t_minishell *shell, char *str);
+void				unset_env(t_minishell *shell, char *key);
 
 /* The exec_binary.c*/
-int			exec_binary(int num, t_minishell *shell);
+int					exec_binary(int num, t_minishell *shell);
 
 /* The builtins.c */
-void		echo(int i, t_minishell *shell);
-void		cd(int i, t_minishell *shell);
-void		pwd(void);
-void		export(int i, t_minishell *shell);
-void		unset(int i, t_minishell *shell);
-void		env(t_minishell *shell);
-void		ft_exit(int i, t_minishell *shell);
+int					echo(int i, t_minishell *shell);
+int					cd(int i, t_minishell *shell);
+int					pwd(int i, t_minishell *shell);
+void				export(int i, t_minishell *shell);
+int					unset(int i, t_minishell *shell);
+int					env(t_minishell *shell);
+int					ft_exit(t_minishell *shell, char *line, t_list *token_list);
+
+/* The ft_error */
+void				ft_error(int i, char *str);
+
+/* The non interactive */
+void				non_interactive_mode(char **array, char **env);
+int					usage(void);
+
+/* The main.c */
+void				free_minishell(t_minishell *shell);
+
+/* The signals.c */
+void				ft_signaux(char *mode);
 
 #endif
